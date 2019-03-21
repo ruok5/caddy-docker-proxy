ba047bdecf2b58524a35c35d1bc8251e4f405961
Brad Rush <brad@fyvm.org>
2019-03-20 21:10:19 -0700

    add entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# exit script if anything fails
+set -e
+
+# load docker secrets into environment variables for Caddy
+export AWS_ACCESS_KEY_ID=$(cat /run/secrets/caddy_aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/caddy_aws_secret_access_key)
+
+# Dockerfile RUN command will take the place of this script (PID 1)
+exec "$@"
